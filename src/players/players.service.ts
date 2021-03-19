e6b42fed1714956a8765d9c5d66caf900adb19c5
Alan Maxwell <alanmaxwellantunes@gmail.com>
2021-03-18 23:05:29 -0300

    Add createPlayer method saving locally

--- src/players/players.service.ts
+++ src/players/players.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { v4 as uuidv4 } from 'uuid';
+import { CreatePlayerDto } from './dto/create-player.dto';
+import { Player } from './interfaces/player.interface';
 
 @Injectable()
-export class PlayersService {}
+export class PlayersService {
+  private players: Player[] = [];
+
+  private readonly logger = new Logger(PlayersService.name);
+
+  async createPlayer(createPlayerDto: CreatePlayerDto): Promise<void> {
+    await this.create(createPlayerDto);
+  }
+
+  private create(createPlayerDto: CreatePlayerDto): void {
+    const { name, phone, email } = createPlayerDto;
+
+    const player: Player = {
+      _id: uuidv4(),
+      phone,
+      email,
+      name,
+      ranking: 'A',
+      position: 1,
+      photo: 'www.google.com.br/player.jpg',
+    };
+    this.logger.log(`createPlayerDto: ${JSON.stringify(player)}`);
+    this.players.push(player);
+  }
+}
