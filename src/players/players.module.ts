c5dbec566b761dd27e78275eb74412a03d9409d7
Alan Maxwell <alanmaxwellantunes@gmail.com>
2021-03-21 15:16:14 -0300

    Import MongooseModule for feature on players module

--- src/players/players.module.ts
+++ src/players/players.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PlayersController } from './players.controller';
 import { PlayersService } from './players.service';
+import { PlayerSchema } from './schemas/player.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Player', schema: PlayerSchema }]),
+  ],
   controllers: [PlayersController],
   providers: [PlayersService],
 })
