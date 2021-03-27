87df5f5d7dfe61a82e100d44fe06504ff149f417
Alan Maxwell <alanmaxwellantunes@gmail.com>
2021-03-27 17:05:19 -0300

    Add createCategory method

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { CategoriesService } from './categories.service';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { Category } from './interfaces/category.interface';
 
-@Controller('categories')
-export class CategoriesController {}
+@Controller('api/v1/categories')
+export class CategoriesController {
+  constructor(private readonly categoriesService: CategoriesService) {}
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  async createCategory(
+    @Body() createCategoryDto: CreateCategoryDto,
+  ): Promise<Category> {
+    return await this.categoriesService.createCategory(createCategoryDto);
+  }
+}
--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { Category } from './interfaces/category.interface';
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+  constructor(
+    @InjectModel('Category') private readonly categoryModel: Model<Category>,
+  ) {}
+
+  async createCategory(
+    createCategoryDto: CreateCategoryDto,
+  ): Promise<Category> {
+    const { category } = createCategoryDto;
+
+    const alreadyExists = await this.categoryModel.findOne({ category }).exec();
+
+    if (alreadyExists) {
+      throw new BadRequestException(`Category ${category} already exists.`);
+    }
+
+    const newCategory = new this.categoryModel(createCategoryDto);
+
+    return await newCategory.save();
+  }
+}
