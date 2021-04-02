895f6181aa279b216e7d1d2972368a8218fc93ed
Alan Maxwell <alanmaxwellantunes@gmail.com>
2021-04-02 01:01:47 -0300

    Add mongoose module with challenge schema

--- src/challenges/challenges.module.ts
+++ src/challenges/challenges.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ChallengesController } from './challenges.controller';
 import { ChallengesService } from './challenges.service';
+import { ChallengeSchema } from './schemas/challenge.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Challenge', schema: ChallengeSchema }]),
+  ],
   controllers: [ChallengesController],
   providers: [ChallengesService],
 })
