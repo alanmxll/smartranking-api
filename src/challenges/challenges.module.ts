a2bc465a4c4639bbf6e37e999993d796f51d90bd
Alan Maxwell <alanmaxwellantunes@gmail.com>
2021-04-05 21:06:01 -0300

    Import PlayersModule and CategoriesModule

--- src/challenges/challenges.module.ts
+++ src/challenges/challenges.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CategoriesModule } from 'src/categories/categories.module';
+import { PlayersModule } from 'src/players/players.module';
 import { ChallengesController } from './challenges.controller';
 import { ChallengesService } from './challenges.service';
 import { ChallengeSchema } from './schemas/challenge.schema';
@@ -7,6 +9,8 @@ import { ChallengeSchema } from './schemas/challenge.schema';
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'Challenge', schema: ChallengeSchema }]),
+    PlayersModule,
+    CategoriesModule,
   ],
   controllers: [ChallengesController],
   providers: [ChallengesService],
