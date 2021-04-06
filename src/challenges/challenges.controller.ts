6ad4e5cf0530631cec7d097c4bedfbff06474851
Alan Maxwell <alanmaxwellantunes@gmail.com>
2021-04-05 21:05:32 -0300

    Create createChallenge handle

--- src/challenges/challenges.controller.ts
+++ src/challenges/challenges.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { ChallengesService } from './challenges.service';
+import { CreateChallengeDto } from './dto/create-challenge.dto';
+import { Challenge } from './interfaces/challenge.interface';
 
-@Controller('challenges')
-export class ChallengesController {}
+@Controller('api/v1/challenges')
+export class ChallengesController {
+  constructor(private readonly challengesService: ChallengesService) {}
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  async createChallenge(
+    @Body()
+    createChallengeDto: CreateChallengeDto,
+  ): Promise<Challenge> {
+    return await this.challengesService.createChallenge(createChallengeDto);
+  }
+}
